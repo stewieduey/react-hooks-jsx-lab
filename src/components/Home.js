93e466551af52d05f56c5aac3a4bb939b3c9ff4c
DuMark <dumark.allman@yahoo.com>
2023-12-10 13:34:38 -0500

    [a doozy}

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
-function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+const homeStyle = {
+  color: 'firebrick',
 }
 
+
+function Home() {
+  // update the JSX being returned!
+  return( 
+  <div id= "home"> 
+  <h1 style={homeStyle}> { name } is a Web Developer from { city } </h1>
+  </div>
+
+  )}
+
 export default Home;
+
+
+
